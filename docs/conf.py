a4a68d5acf6c870cdad62b99e64fffc5d659d56b
wemos <support@wemos.cc>
2017-10-11 19:38:33 +0800

    test

--- docs/conf.py
+++ docs/conf.py
@@ -37,14 +37,7 @@ import sys
 import sphinx_rtd_theme
 from recommonmark.parser import CommonMarkParser
 
-sys.path.insert(0, os.path.abspath('..'))
-sys.path.append(os.path.dirname(__file__))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "readthedocs.settings.dev")
 
-from django.conf import settings
-
-import django
-django.setup()
 
 
 sys.path.append(os.path.abspath('_ext'))
